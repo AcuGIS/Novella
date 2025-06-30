50fc1760774bc1fbf173fe3dd4b128081bf15fe6
AcuGIS <david@davidghedini.com>
2025-06-30 08:07:15 +0300

    Docs

--- migrations/010_add_metadata_xml_to_oai_records.sql
+++
@@ -1,5 +0,0 @@
--- Add metadata_xml column to oai_records table
-ALTER TABLE oai_records ADD COLUMN metadata_xml XML;
-
--- Create an index on metadata_xml for faster lookups
-CREATE INDEX IF NOT EXISTS idx_oai_records_metadata_xml ON oai_records USING gin (metadata_xml); 
--- public/basic.php
+++
@@ -1,4 +0,0 @@
-<?php
-// No strict types, no dependencies, just basic PHP
-echo "Basic PHP test";
-phpinfo(); 
